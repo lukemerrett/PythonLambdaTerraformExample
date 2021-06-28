5d0439c853cfae9a257c4c8938cbb3c815917aa2
Luke Merrett <lukeamerrett@gmail.com>
2021-06-28 10:43:19 +0100

    Base terraform file

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  role             = "${aws_iam_role.lambda_exec_role.arn}"
+  handler          = "src.lambda.handler"
+  runtime          = "python3.9"
+  filename         = "package.zip"
+  function_name    = "minimal_lambda_function"
+  source_code_hash = "${base64sha256(file("lambda.zip"))}"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name        = "lambda_exec"
+  path        = "/"
+  description = "Allows Lambda Function to call AWS services on your behalf."
+
+  assume_role_policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+    EOF
+}
